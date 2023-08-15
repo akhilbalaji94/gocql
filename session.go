b66587308b7e97cb7c39910b5e1f0ce4c3aff891
Akhil Balaji <balaji.akhil@gmail.com>
2023-08-15 16:01:00 -0500

    Update session.go

--- session.go
+++ session.go
@@ -18,7 +18,7 @@ import (
 	"time"
 	"unicode"
 
-	"github.com/gocql/gocql/internal/lru"
+	"akhilbalaji94/gocql/gocql/internal/lru"
 )
 
 // Session is the interface used by users to interact with the database.
